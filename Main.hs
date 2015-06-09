1c932eae83b5af0e057ee6525c6226af702b089c
Samuel Painter <sam.foy.painter@gmail.com>
2015-06-09 00:11:44 -0400

    used haskeline for more interactive repl

--- Main.hs
+++ Main.hs
@@ -7,23 +7,27 @@ import Environment
 import System.Environment
 import System.IO
 import LispPrimitives
+import System.Console.Haskeline
 
-flushStr :: String -> IO ()
-flushStr str = putStr str >> hFlush stdout
-
-readPrompt :: String -> IO String
-readPrompt prompt = flushStr prompt >> getLine
+-- flushStr :: String -> IO ()
+-- flushStr str = putStr str >> hFlush stdout
+--
+-- readPrompt :: String -> IO String
+-- readPrompt prompt = flushStr prompt >> getLine
 
 evalString :: Env -> String -> IO String
 evalString env expr = runIOThrows $ liftM show $ liftT (lispParse expr) >>= eval env
 
-evalAndPrint :: Env -> String -> IO ()
-evalAndPrint env expr = evalString env expr >>= putStrLn
+-- evalAndPrint :: Env -> String -> IO ()
+-- evalAndPrint env expr = evalString env expr >>= putStrLn
 
-until_ :: Monad m => (a -> Bool) -> m a -> (a -> m()) -> m ()
-until_ predicate prompt action = do
-  result <- prompt
-  unless (predicate result) $ action result >> until_ predicate prompt action
+evalAndPrint' :: Env -> String -> InputT IO ()
+evalAndPrint' env expr = liftIO (evalString env expr) >>= outputStrLn
+
+-- until_ :: Monad m => (a -> Bool) -> m a -> (a -> m()) -> m ()
+-- until_ predicate prompt action = do
+--   result <- prompt
+--   unless (predicate result) $ action result >> until_ predicate prompt action
 
 runOne :: [String] -> IO ()
 runOne exprs = do
@@ -31,10 +35,26 @@ runOne exprs = do
   runIOThrows (liftM show $ eval env (List [Atom "load", String (head exprs)]))
       >>= hPutStrLn stderr
 
-runRepl :: IO ()
-runRepl = primitiveBindings >>= until_ (== "quit") (readPrompt "Lisp>>> ") . evalAndPrint
+-- runRepl :: IO ()
+-- runRepl = primitiveBindings >>= until_ (== "quit") (readPrompt "Lisp>>> ") . evalAndPrint
+
+runRepl' :: IO ()
+runRepl' = do
+  env <- primitiveBindings
+  runInputT defaultSettings (loop env)
+  where
+    loop :: Env -> InputT IO ()
+    loop env = do
+      minput <- getInputLine "Lisp>>> "
+      case minput of
+        Nothing -> return ()
+        Just "quit" -> return ()
+        Just "" -> loop env
+        Just input -> do
+          evalAndPrint' env input
+          loop env
 
 main :: IO ()
 main = do
   args <- getArgs
-  if null args then runRepl else runOne args
+  if null args then runRepl' else runOne args
